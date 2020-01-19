67e5b8dab58167c8b530745bf46c963c9d5b4837
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-01-19 21:08:15 +0000

    Added whitelist functions

--- listeners/whitelistRemoveOnLeave.js
+++ listeners/whitelistRemoveOnLeave.js
@@ -13,57 +13,23 @@ class WhitelistRemoveOnLeave extends Listener {
 	}
 
 	async exec(member) {
-		let bot_id = this.client.user.id;
-		let whitelist_channel, console_channel;
+		let ch_whitelist, ch_console;
 		member.guild.channels.forEach(async function(channel) {
-			if (channel.name == "minecraft-info") {
-				whitelist_channel = channel;
+			if (channel.name == "whitelist") {
+				ch_whitelist = channel;
 			} else if (channel.name == "server-console") {
-				console_channel = channel;
+				ch_console = channel;
 			}
 		});
 
-		if (whitelist_channel && console_channel) {
-			let messages = await whitelist_channel.fetchMessages(10);
-			messages.forEach(function(msg) {
-				if (msg.author.id == bot_id) {
-					msg.embeds.forEach(function(embed) {
-						if (embed.type == "rich") {
-							let fields = embed.fields;
-							if (fields[0].name == "Whitelist") {
-								let people, names;
-								if (fields[1].name == "Person") {
-									people = fields[1].value.split("\n");
-									if (fields[2].name == "Nickname") {
-										names = fields[2].value.split("\n");
-
-										let remove;
-										for (let i = 0; i < people.length; i++) {
-											if (`<@${member.id}>` == people[i]) {
-												remove = i;
-											}
-										}
-
-										if (remove != null) {
-											console_channel.send(`whitelist remove ${names[remove]}`);
-											names.splice(remove, 1);
-											people.splice(remove, 1);
-
-											msg.edit(
-												new Discord.RichEmbed()
-												.setColor(embed.color)
-												.addField("Whitelist", "Type `/whitelist [nickname]` to add a name to the list.\nOnly one nickname is allowed per person.)")
-												.addField("Person", people.join("\n"), true)
-												.addField("Nickname", names.join("\n"), true)
-											);
-										}
-									}
-								}
-							}
-						}
-					});
+		if (ch_whitelist && ch_console) {
+			let messages = await ch_whitelist.fetchMessages(100);
+			for (let m of messages) {
+				if (m[1].author.id == member.id) {
+					await ch_console.send(`whitelist remove ${m[1].content}`)
+					await m[1].delete()
 				}
-			});
+			}
 		}
 	}
 }
