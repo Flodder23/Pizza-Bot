2e1e67e2c9f44f19fbccfcc4930e2dba656a88b2
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-08-03 19:01:59 +0100

    fixed error in assuming server-info channel had been found

--- listeners/ready.js
+++ listeners/ready.js
@@ -20,9 +20,12 @@ class ReadyListener extends Listener {
 			console.log("Started in normal mode.");
 		}
 		for (let [, guild] of this.client.guilds.cache.filter(g => this.client.testMode != (g.name != "Lonely Joe"))) {
-			let messages = await guild.channels.cache.find(c => c.name == "server-info").messages.fetch({ limit: 100 })
-			if (messages.some(m => m.content.startsWith("**ROLES**"))) {
-				console.log(`Found roles message for ${guild.name}`);
+			let ch_server_info = guild.channels.cache.find(c => c.name == "server-info")
+			if (ch_server_info) {
+				let messages = await ch_server_info.messages.fetch({ limit: 100 })
+				if (messages.some(m => m.content.startsWith("**ROLES**"))) {
+					console.log(`Found roles message for ${guild.name}`);
+				}
 			}
 			if (guild.channels.cache.some(g => g.name == "whitelist")) {
 				console.log(`Found whitelist channel for ${guild.name}`)
