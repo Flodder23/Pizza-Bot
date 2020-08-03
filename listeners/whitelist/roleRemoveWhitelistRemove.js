5c3d019da5bc333216766f03554abe1fb5d3d7ad
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-08-03 13:57:13 +0100

    Cleaned code for the whitelist listeners

--- listeners/whitelist/roleRemoveWhitelistRemove.js
+++ listeners/whitelist/roleRemoveWhitelistRemove.js
@@ -13,27 +13,12 @@ class roleRemoveWhitelistRemoveListener extends Listener {
 
 	async exec(oldMember, newMember) {
 		if (this.client.testMode == (oldMember.guild.name == "Lonely Joe")) {
-			for (let role of oldMember.roles.cache) {
-				if (typeof newMember.roles.cache.get(role[0]) == "undefined") {
-					if (role[1].name == "Minecraft") {
-						let ch_whitelist, ch_console;
-						for (let channel of newMember.guild.channels.cache) {
-							if (channel[1].name == "whitelist") {
-								ch_whitelist = channel[1];
-							} else if (channel[1].name == "server-console") {
-								ch_console = channel[1];
-							}
-						}
-						if (ch_whitelist && ch_console) {
-							let messages = await ch_whitelist.messages.fetch({ limit: 100 })
-							for (let m of messages) {
-								if (m[1].author.id == newMember.id) {
-									await m[1].delete()
-								}
-							}
-						}
-					}
-					break
+			let roleRemoved = await oldMember.roles.cache.find(r => !newMember.roles.cache.has(r.id))
+			if (roleRemoved && roleRemoved.name == "Minecraft") {
+				let ch_whitelist = await newMember.guild.channels.cache.find(c => c.name == "whitelist")
+				let usernames = await ch_whitelist.messages.fetch({ limit: 100 })
+				for (let [, u] of usernames.filter(m => m.author.id == newMember.id)) {
+					await u.delete({ reason: "Role Removed" })
 				}
 			}
 		}
