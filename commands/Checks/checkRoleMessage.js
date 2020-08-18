04b90a0e0c620003db3f5326306436d9c8629da2
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-08-18 13:00:05 +0100

    checkRoleMessage command now looks for the roles message if a link to a message isn't provided

--- commands/Checks/checkRoleMessage.js
+++ commands/Checks/checkRoleMessage.js
@@ -7,7 +7,7 @@ const commandInfo = {
 	args: [{id: "messageLink", type: "string"}],
 	description: {
 		short: "Checks that the bot can find & see the react message in this server",
-		extend: "To get the link to a message, right-click on it (or click the 3 dots at the top-right of the message) and click `Copy Message Link`",
+		extend: "If no link to a message is given, the bot will try to find it itself.\nTo get the link to a message, right-click on it (or click the 3 dots at the top-right of the message) and click `Copy Message Link`",
 	}
 }
 
@@ -24,24 +24,42 @@ class CheckRoleMessageCommand extends Command {
 	}
 
 	async exec(message, args) {
-		let msg = linkToMessage(args.messageLink, message.guild)
-		if (typeof msg == "string") {
-			return await message.channel.send(msg)
-		}
-		let output = []
-		if (msg.author.id != this.client.user.id) {
-			output.push("Message not sent by this bot - to get bot to send a message, use the `echo` command")
-		}
-		if (msg.channel.name != "server-info") {
-			output.push("Message must be in channel called `server-info`")
-		}
-		if (!msg.content.startsWith("**ROLES**")) {
-			output.push("Message must start with \"**ROLES**\" (to get bold effect, type `**ROLES**`)")
-		}
-		if (output.length == 0) {
-			return await message.channel.send("Roles message is valid.")
+		if (args.messageLink) {
+			let msg = linkToMessage(args.messageLink, message.guild)
+			if (typeof msg == "string") {
+				return await message.channel.send({ embed: { description: msg } })
+			}
+			let output = []
+			if (msg.author.id != this.client.user.id) {
+				output.push("Message not sent by this bot - to get bot to send a message, use the `echo` command.")
+			}
+			if (msg.channel.name != "server-info") {
+				output.push("Message must be in channel called `server-info`.")
+			}
+			if (!msg.content.startsWith("**ROLES**")) {
+				output.push("Message must start with \"**ROLES**\" (to get bold effect, type `**ROLES**`)")
+			}
+			if (output.length == 0) {
+				return await message.channel.send({ embed: { description: "Roles message is valid." } })
+			} else {
+				return await message.channel.send({ embed: { description: output.join("\n") } })
+			}
 		} else {
-			return await message.channel.send(output.join("\n"))
+			let roleMessages = []
+			let channels = message.guild.channels.cache.filter(c => c.name == "server-info" && c.type == "text")
+			if (channels.size == 0) {
+				return await message.channel.send({ embed: { description: "Could not find channel named `server-info`." } })
+			}
+			for (let [, c] of channels) {
+				roleMessages.push(...c.messages.cache.filter(m => m.author.id == this.client.user.id && m.content.startsWith("**ROLES**")).array())
+			}
+			if (roleMessages.length == 0) {
+				return await message.channel.send({ embed: { description: `Could not find roles message in ${channels.map(c => c.toString()).join(", ")}.` } })
+			} else if (roleMessages.length == 1) {
+				return await message.channel.send({ embed: { description: `Roles message found. [goto](${roleMessages[0].url})` } })
+			} else {
+				return await message.channel.send({ embed: { description: `Warning - multiple role messages found. ${roleMessages.map(m => `[goto](${m.url})`).join(" ")}` } })
+			}
 		}
 
 	}
