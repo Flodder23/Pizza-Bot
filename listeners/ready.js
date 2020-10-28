52f452845714e63073319c93d93a86539d924998
Joe Gibson <joe.gibby@hotmail.co.uk>
2020-10-28 13:38:19 +0000

    now fetches members in ready

--- listeners/ready.js
+++ listeners/ready.js
@@ -23,6 +23,7 @@ class ReadyListener extends Listener {
 			output += "Started in normal mode\n"
 		}
 		for (let [, guild] of this.client.guilds.cache.filter(g => this.client.testMode != (g.name != "Lonely Joe"))) {
+			await guild.members.fetch()
 			let ch_server_info = guild.channels.cache.find(c => c.name == "server-info")
 			if (ch_server_info) {
 				let messages = await ch_server_info.messages.fetch({ limit: 100 })
